9aaf9efab880a8fa7b8203323ff0feec9f13dd6b
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2022-10-21 09:00:04 -0500

    Descarga

--- js/aes.js
+++ js/aes.js
@@ -1,7 +1,12 @@
 const form = document.getElementById('Form')
 const descargar = document.getElementById('Descargar')
-const blob = new Blob([cifrarTxt()])
-descargar.href=URL.createObjectURL(blob)
+
+document.getElementById('Tcifrar').addEventListener('click', (e)=>{
+    e.preventDefault()
+    cifrarTxt()
+
+})
+
 var pass =""
 var txt =""
 var cifradotxt
@@ -9,13 +14,27 @@ var descifradotxt
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
 })
-function cifrarTxt(){
+const cifrarTxt = ()=>{
     pass =document.getElementById("Clave").value
     txt = document.getElementById('Texto').value
     cifradotxt = CryptoJS.AES.encrypt(txt,pass);
+    const archivo = new Blob([cifradotxt], {type:'application/x-www-form-urlencoded'})
+    const url = window.URL.createObjectURL(archivo)
+    const linktxt = document.createElement('a')
+    const linkpdf = document.createElement('a')
+    const linkdocx= document.createElement('a')
+    linktxt.href = url
+    linktxt.download = "cifrado.txt"
+    linktxt.click()
+    linkpdf.href = url
+    linkpdf.download = "cifrado.pdf"
+    linkpdf.click()
+    linkdocx.href = url
+    linkdocx.download = "cifrado.docx"
+    linkdocx.click()
+    URL.revokeObjectURL(url)
     document.getElementById('Resultadotxt').innerHTML=cifradotxt
     document.getElementById('Resultadotxt').style.display='block'
-    return cifradotxt
 }
 function descifrarArc(){
     
